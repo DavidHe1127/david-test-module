f2360ddc72c1249938c5d1a3e2f8f15d04581024
David He <jialhe85@gmail.com>
2020-03-30 16:26:05 +1100

    chore(test-chore): this is a chore

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ const semantic_release = 'it\s an awesome tool';
 const aws = 'i need to pass SA Pro';
 const aws_1 = 'i have already passed sa associate';
 const game = 'i should stop playing games';
+
+const chore = 'test chore';
