8b876ed09dfd57c5fe627497e60fe8d7610334a4
David He <jialhe85@gmail.com>
2020-03-30 16:55:18 +1100

    add

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 // semantic release config
-
+// This test run was triggered on the branch chore/tidy-up, while semantic-release is configured to only publish from master, prerelease/support-prerelease-glob-name, therefore a new version won’t be published.
 module.exports = {
   branches: [
     'master',
