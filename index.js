f8c41537ebc4b1db28aa77f37e3b7fb4120e2b2e
David He <jialhe85@gmail.com>
2020-03-31 17:34:54 +1100

    feat(dave): prerelease 1.7.0

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const aws = 'i need to pass SA Pro';
 const aws_1 = 'i have already passed sa associate';
 const game = 'i should stop playing games';
 
-const nextVersion = '1.6.0'
+const nextVersion = '1.7.1'
